e98d976468cdf3f93518eb0dc368de491bed79d7
Brian Lin <brianlin@Brians-MacBook-Pro.local>
2021-01-16 20:57:03 -0500

    background works! + babel/styled comp

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 import { withSiteData } from 'react-static'
-//
-import logoImg from '../logo.png'
+
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 export default withSiteData(() => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" style={{ display: 'block', margin: '0 auto' }} />
+    
   </div>
 ))

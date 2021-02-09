9d03407fcdde3019d356238ba69b0d3ddc59146b
briguy52 <briguy52@gmail.com>
2021-02-08 19:40:21 -0500

    added a plain white card to home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
 import { withSiteData } from 'react-static'
-//
+
 
 export default withSiteData(() => (
-  <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
+  <div className="home-container">
+    <div className="welcome-card">
+      Hello
+    </div>
   </div>
 ))

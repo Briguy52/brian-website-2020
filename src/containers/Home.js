9bde360a95fc87a91c758367c0af274b852e2663
briguy52 <briguy52@gmail.com>
2021-02-08 20:37:10 -0500

    basic card works

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { withSiteData } from 'react-static'
 
+
 export default withSiteData(() => (
   <div className="home-container">
     <div className="welcome-card">
+      <div className="text-area">
+        <h1>
+          Hi, I'm Brian!
+        </h1>
+
+        <p>
+        I’m a software engineer at Capital One
+        by day and a stray cat loving, long distance
+        running, typewriter poet by night.
+        </p>
+      </div>
+
       
     </div>
   </div>

b0ee114f145ab166da738b90ca91390b84e02291
briguy52 <briguy52@gmail.com>
2021-02-17 21:38:49 -0500

    website looks DOPE!

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,15 +6,25 @@ export default withSiteData(() => (
   <div className="home-container">
     <div className="welcome-card">
       <div className="text-area">
-        <h1>
-          Hi, I'm Brian!
+        <h1 className="card-title">
+          Hello, I'm Brian
         </h1>
 
-        <p>
-        I’m a software engineer at Capital One
-        by day and a stray cat loving, long distance
-        running, typewriter poet by night.
-        </p>
+        <div className="card-text">
+          <p >
+            I'm a software engineer.
+          </p>
+
+          <p>
+            I'm also a writer, long distance runner, and slice of life anime fan.
+          </p>
+
+          <p>
+            You can read my blog on <a href="https://brianmlin.medium.com/">Medium</a> and check out my photos, sketches, and poetry on <a href="https://www.instagram.com/brianmlin/">Instagram</a>.
+          </p>
+        </div>
+
+
       </div>
 
       
